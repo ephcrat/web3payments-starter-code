c4122fee1e9ceea1ce0ccbe8e81bc3d16333518a
Alejandro <alejandromanuelhernandez@gmail.com>
2022-11-08 01:21:52 -0300

    initial boilerplate

---
+++ pages/_app.tsx
@@ -0,0 +1,48 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../theme";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [chain.goerli],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+      apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY,
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          showRecentTransactions={true}
+          coolMode
+        >
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
